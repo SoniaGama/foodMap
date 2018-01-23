c380a4b3c8c2c285fc5837a126217e01d9840bbf
SoniGama <abinyuuu@gmail.com>
2018-01-22 18:22:38 -0600

    avances

--- js/app.js
+++ js/app.js
@@ -2,11 +2,10 @@ $(document).ready(loadPage);
 var $searcher = $('#input-select');
 
 
-
 // mapa de google maps
 function mapFood() {
     var mapOptions = {
-        center: new google.maps.LatLng(19.432608, -99.133208),
+        center: new google.maps.LatLng(19.4203024, -99.1631142),
         zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
@@ -18,50 +17,107 @@ function loadPage() {
    // comienza con una vista splash y se remueve la clase d-none para que
    // muestre el contenido
   $(".section-fade-out").fadeOut(2000, function(){
-    $(".main-section").removeClass("d-none");
+    $(".main-section").removeClass("hidden");
   });
+
   // Asignamos evento y llamamos a la funcion que va a filtrar los restaurantes
   $searcher.keyup(filterOptions);
 }
 
 // console.log($foodMap[2]['type']);
+//
+// function filterOptions () {
+//   var food = []
+//   // guardamos el value de input
+//   var $valInput = $searcher.val().toLowerCase();
+//   // console.log($valInput);
+//
+//   if($('#input-select').val().trim().length > 0){
+//
+//     var filtered = $foodMap.filter(function(){
+//       for (var i = 0; i < $foodMap.length; i++) {
+//         var places = $foodMap[i]['type'];
+//          // console.log(places);
+//          // console.log(places.type.toLowerCase().indexOf($valInput) >= 0);
+//          // var result = places.type.toLowerCase().indexOf($valInput) >= 0;
+//          var result = places.toLowerCase().indexOf($valInput) >= 0;
+//          // food.push(places);
+//          // console.log(food);
+//           // return result;
+//       }
+//       console.log(places);
+//       // console.log(places.type.toLowerCase().indexOf($valInput) >= 0);
+//      // return places.type.toLowerCase().indexOf($valInput) >= 0;
+//   });
+//   // console.log(filtered);
+//   $('#container-place').empty();
+//   filtered.forEach(function(){
+//     // paintOptions(places);
+//   });
+//   }
+// }
+
 
 function filterOptions () {
-  var food = []
-  // guardamos el value de input
+
   var $valInput = $searcher.val().toLowerCase();
-  // console.log($valInput);
+  var food = $foodMap2[$valInput];
+  // console.log(food);
 
   if($('#input-select').val().trim().length > 0){
-
-    var filtered = $foodMap.filter(function(){
-      for (var i = 0; i < $foodMap.length; i++) {
-        var places = $foodMap[i]['type'];
-         // console.log(places);
-         // console.log(places.type.toLowerCase().indexOf($valInput) >= 0);
-         // var result = places.type.toLowerCase().indexOf($valInput) >= 0;
-         var result = places.toLowerCase().indexOf($valInput) >= 0;
-         // console.log(result);
-         // return result;
+    for (var i in $foodMap2) {
+      var places = i
+      if(places.toLowerCase() === $valInput){
+         paintOptions(food);
       }
-       food.push(places);
-      console.log(food);
-
-      // console.log(places.type.toLowerCase().indexOf($valInput) >= 0);
-     // return places.type.toLowerCase().indexOf($valInput) >= 0;
-  });
-  $('#container-place').empty();
-  filtered.forEach(function(){
-    // paintOptions(places);
-  });
+    }
   }
 }
 
-// function paintOptions(places){
-//   if(places['name'] === "Mexicana"){
-//     console.log('siii');
-//   } else {
-//     console.log('nooo');
-//   }
-//    // var $containerFoods = $("<article />", {"class":"container-place"});
-// }
+
+    //this - data target--
+
+
+  //   var filtered = $foodMap2.filter(function(){
+  //     for (var i in $foodMap2) {
+  //       var places = $foodMap2[i]
+  //       console.log(places);
+  //
+  //        // var result = places.toLowerCase().indexOf($valInput) >= 0;
+  //
+  //     }
+  //
+  // });
+  // console.log(filtered);
+  // $('#container-place').empty();
+  // filtered.forEach(function(){
+    // paintOptions(places);
+  // });
+
+function paintOptions(food){
+
+  var $container = $(".container-place");
+  var modalTitle = $(".modal-title");
+  var modalBody = $(".modal-body");
+
+  $container.empty();
+  // console.log(food);
+
+  for (var i = 0; i < food.length; i++) {
+    var restaurants = food[i];
+    // console.log(restaurants);
+    var images = $('<img>');
+    var srcImg = restaurants['img'];
+    images.attr('src', srcImg);
+    $container.append(images);
+
+    paintModal($container);
+    // console.log($container);
+  }
+  // console.log(restaurants);
+  // var $containerFoods = $("<article />", {"class":"container-place"});
+}
+
+function paintModal ($container) {
+  
+}
--- js/data.js
+++ js/data.js
@@ -1,36 +1,81 @@
-var $foodMap = [
+// var $foodMap = {
+//   //En estos objetos por restaurante debe estar especificada
+//   //la lat y log que va a pasar como argumento a la funcion de google maps
+//     {
+//       'type': 'Mexicana',
+//       'name': 'La Casa de Toño',
+//       'slogan': 'La Casa del Buen Comer',
+//       'address': 'Tonala 20 Col. Roma',
+//       'prices': '$$',
+//       'services': 'Servicio a domicilio'
+//     },
+//     {
+//       'type': 'Mexicana',
+//       'name': 'Coco',
+//       'slogan': 'No llores!!',
+//       'address': 'Tonala 35 Col. Roma',
+//       'prices': '$$',
+//       'services': 'No hay servicio a domicilio'
+//     },
+//     {
+//       'type': 'Italiana',
+//       'name': "Italianni's",
+//       'slogan': 'La Tradizione di Compartire',
+//       'address': 'Tonala 40 Col. Roma',
+//       'prices': '$$$',
+//       'services': 'Servicio a domicilio'
+//     },
+//     {
+//       'type': 'Italiana',
+//       'name': 'Palazzo',
+//       'slogan': 'Cucina Italiana',
+//       'address': 'Tonala 5 Col. Roma',
+//       'prices': '$$',
+//       'services': 'Servicio a domicilio'
+//     }
+// };
+
+var $foodMap2 = {
   //En estos objetos por restaurante debe estar especificada
   //la lat y log que va a pasar como argumento a la funcion de google maps
-    {
-      'type': 'Mexicana',
-      'name': 'La Casa de Toño',
-      'slogan': 'La Casa del Buen Comer',
-      'address': 'Tonala 20 Col. Roma',
-      'prices': '$$',
-      'services': 'Servicio a domicilio'
-    },
-    {
-      'type': 'Mexicana',
-      'name': 'Coco',
-      'slogan': 'No llores!!',
-      'address': 'Tonala 35 Col. Roma',
-      'prices': '$$',
-      'services': 'No hay servicio a domicilio'
-    },
-    {
-      'type': 'Italiana',
-      'name': "Italianni's",
-      'slogan': 'La Tradizione di Compartire',
-      'address': 'Tonala 40 Col. Roma',
-      'prices': '$$$',
-      'services': 'Servicio a domicilio'
-    },
-    {
-      'type': 'Italiana',
-      'name': 'Palazzo',
-      'slogan': 'Cucina Italiana',
-      'address': 'Tonala 5 Col. Roma',
-      'prices': '$$',
-      'services': 'Servicio a domicilio'
-    }
-];
+   'mexicana': [
+     {
+       'type': 'Comida Mexicana',
+       'name': 'La Casa de Toño',
+       'slogan': 'La Casa del Buen Comer',
+       'address': 'Tonala 20 Col. Roma',
+       'prices': '$$',
+       'services': 'Servicio a domicilio',
+       'img': 'images/mexicana1.jpg'
+     },
+     {
+       'type': 'Comida Mexicana',
+       'name': 'Coco',
+       'slogan': 'No llores!!',
+       'address': 'Tonala 35 Col. Roma',
+       'prices': '$$',
+       'services': 'No hay servicio a domicilio',
+        'img': 'images/mexicana2.jpg'
+     }
+   ],
+   'italiana': [
+     {
+       'type': 'Comida Italiana',
+       'name': "Italianni's",
+       'slogan': 'La Tradizione di Compartire',
+       'address': 'Tonala 40 Col. Roma',
+       'prices': '$$$',
+       'services': 'Servicio a domicilio',
+        'img': 'images/italiana1.jpg'
+     },
+     {
+       'type': 'Comida Italiana',
+       'name': 'Palazzo',
+       'slogan': 'Cucina Italiana',
+       'address': 'Tonala 5 Col. Roma',
+       'prices': '$$',
+       'services': 'Servicio a domicilio',
+        'img': 'images/italiana2.jpg'
+     }
+   ]
+};
